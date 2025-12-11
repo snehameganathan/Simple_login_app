c08e1e57986ef4d86631bd78f81112b393875b89
snehameganathan <snehameganath@gmail.com>
2025-12-11 20:11:19 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(
       "https://simple-login-app-three.vercel.app/", // optionally add your Vercel URL
     ],
     methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
   })
 );
